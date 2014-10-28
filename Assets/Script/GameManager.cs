efc8ab9b543d5474cbdf32c31b99c986f088fba4
ayame9joe <ayame9joe@gmail.com>
2014-10-28 15:39:40 +0800

    Upload the whole project

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+
+	public AudioSource heartBeating;
+	public GameObject OtherPrefab;
+	int numOfOthers = 10;
+	//bool isGoingRight = true;
+	float beatingRate = 1.0f;
+
+	public Image Heart;
+	int cupsOfBeer = 0;
+	public Text CupsOfBeer;
+	public Text TriedTimes;
+
+
+	int numsOfHeartBeating = 0;
+	float beatingRateSeed = 0f;
+
+	public static int triedTimes = 0; 
+
+	public static float widthOfScreen = 960f;
+	public static float heightOfScreen = 640f;
+
+	// Use this for initialization
+	void Start () {
+
+		/*for (int i = 0; i < numOfOthers; i++)
+		{
+			GameObject go = Instantiate(OtherPrefab, new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-150, 150),0), transform.rotation) as GameObject;
+			go.transform.parent = GameObject.Find("Canvas").transform;
+			go.transform.localPosition = new Vector3(Random.Range(-200.0f, 200.0f), Random.Range(-150, 150),0);
+		}*/
+
+		GameObject[] others = GameObject.FindGameObjectsWithTag ("Other");
+		for (int i = 0; i < others.Length; i++) {
+			others[i].transform.localPosition = new Vector3(Random.Range(-widthOfScreen / 2, widthOfScreen / 2), Random.Range(-heightOfScreen / 2, heightOfScreen / 2),0);
+			
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CupsOfBeer.text = "*  " + cupsOfBeer.ToString ();
+		TriedTimes.text = "You've tried " + triedTimes + " times";
+		beatingRate = (GameObject.Find ("The One").transform.localPosition.x + widthOfScreen / 2) * 0.01f;
+	
+
+		InvokeRepeating("HeartBeating", beatingRate, 1);
+		if (beatingRate < cupsOfBeer * 0.1f) {
+			beatingRate = cupsOfBeer * 0.1f;	
+		}
+
+		GameObject[] others = GameObject.FindGameObjectsWithTag ("Other");
+
+
+			for (int i = 0; i < others.Length; i++) {
+						if (others [i].GetComponent<Others> ().IsFacingRight)
+			{
+								others [i].transform.Translate (new Vector3 (others [i].GetComponent<Others> ().Speed, 0, 0));
+				others [i].transform.localScale = new Vector3 (-1, 1, 1);
+						
+			}	
+				else 
+			{
+				others[i].transform.Translate (new Vector3 (-others[i].GetComponent<Others>().Speed, 0, 0));
+				others [i].transform.localScale = new Vector3 (1, 1, 1);
+			}
+					
+			}
+	}
+
+	void HeartBeating ()
+	{
+		//Debug.Log("Heart Beat!");
+		//Debug.Log (beatingRate);
+		numsOfHeartBeating++;
+		heartBeating.Play ();
+		if (numsOfHeartBeating % 2 == 1)
+			
+		{
+			
+			Heart.rectTransform.localScale = new Vector3 (1.2f, 1.2f, 1);
+			
+		}
+		
+		else
+			
+			Heart.rectTransform.localScale = new Vector3 (1, 1, 1);
+	
+
+		CancelInvoke ();
+		
+	}
+
+	public void OnDrink ()
+	{
+		//Debug.Log("Drink");
+		cupsOfBeer++;
+	}
+
+
+}
---
+++ Assets/Script/Others.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Others: MonoBehaviour{
+
+	private bool isFacingRight;
+	private float speed;
+
+	private bool spaceDown = false;
+
+	public static int othersCount = 0;
+	public bool IsFacingRight
+	{
+		get
+		{
+			//Some other code
+			return isFacingRight;
+		}
+		set
+		{
+			//Some other code
+			isFacingRight = value;
+		}
+	}
+
+	public float Speed
+	{
+		get
+		{
+			//Some other code
+			return speed;
+		}
+		set
+		{
+			//Some other code
+			speed = value;
+		}
+	}
+
+	public void Start ()
+	{
+		if (this.name != "The Line" && this.name != "The One") {
+		
+			othersCount++;
+		//	Debug.Log(othersCount);
+
+			int randomOne = Random.Range(1, 2);
+
+			if (othersCount == randomOne)
+			{
+				Debug.Log(randomOne);
+				this.name = "The One";
+			}
+
+		}
+
+		isFacingRight = RandomBool ();
+		if (this.name == "The Line") {
+					
+			speed = 2f;
+				} else {
+						speed = Random.Range (0.5f, 1.5f);
+			this.gameObject.GetComponent<Animator> ().speed = speed * 3;
+				}
+
+
+	}
+
+	public void Update()
+	{
+		if (this.transform.localPosition.x > GameManager.widthOfScreen / 2) {
+						this.isFacingRight = false;		
+		} else if (this.transform.localPosition.x < -GameManager.widthOfScreen / 2) {
+			this.isFacingRight = true;		
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+						spaceDown = true;
+			//Debug.Log("Space Down");
+				} else {
+			spaceDown = false;		
+		}
+
+	}
+
+	public bool RandomBool()
+	{
+		int random = Random.Range (0, 2);
+		if (random == 0) {
+			
+			return false;	
+		} else {
+			return true;		
+		}
+
+
+	}
+
+	void OnMouseDown ()
+	{
+		Debug.Log("This is not the one");
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (this.name == "The Line") {
+		
+			if (other.name == "The One")
+			{
+				//Debug.Log("This is the one");
+
+				if (spaceDown)
+				{
+					Debug.Log("Catch ya!");
+					GameManager.triedTimes++;
+				}
+			}
+
+			else if (other.name == "Other" || other.name == "Other(Clone)")
+			{
+
+				if (spaceDown)
+				{
+					Debug.Log("Wrong person");
+					Destroy(other.gameObject);
+					GameManager.triedTimes++;
+				}
+			}
+		}
+
+	}
+}
